3f3dbfeeaa463fe0893d9ad5180c99c5aadb9be7
Sylvie <1553902582@qq.com>
2025-03-10 08:31:28 +0800

    py接口对接/打开本地文件

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,14 +6,46 @@
       style="width: 240px"
       placeholder="请输入关键词"
     />
-    <el-button type="primary" :icon="Search" circle />
+    <el-button type="primary" :icon="Search" circle @click="a()" />
+    <el-button type="primary" :icon="Search" circle @click="b()" />
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue";
+import { ref, onMounted } from "vue";
 import { Search } from "@element-plus/icons-vue";
 const input = ref("");
+import { openPath } from "@tauri-apps/plugin-opener";
+import axios from "axios";
+
+onMounted(async () => {
+  console.log("111");
+});
+
+const a = async () => {
+  // await openPath("../../public/rust.exe");
+  axios
+    .post("/api/get_all", {
+      table_name: "knowledge_base",
+    })
+    .then((res) => {
+      console.log("res", res.data);
+    });
+};
+const b = async () => {
+  // await openPath("../../public/rust.exe");
+  axios
+    .post("/api/add_knowledge_base", {
+      name: "1",
+      description: "1",
+      encoding_model: "1",
+      is_multimodal: "1",
+      bind_path: "1",
+    })
+    .then((res) => {
+      console.log("res", res.data);
+    });
+};
 </script>
 
 <style scoped>
@@ -29,8 +61,8 @@ const input = ref("");
   height: 50px;
 }
 
-.el-button.is-circle{
-  border-radius:  0 200px 200px 0 !important;
+.el-button.is-circle {
+  border-radius: 0 200px 200px 0 !important;
   height: 52px;
   width: 50px;
 }
